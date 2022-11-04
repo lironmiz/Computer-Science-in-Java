0cb231a6ce2603bf94092807a2e9f8ba49e8c6dc
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2022-11-04 05:43:10 +0200

    fix file

--- Car.java
+++ Car.java
@@ -19,20 +19,20 @@ public class Car
       int lengthOfId = String.valueOf(id).length();
       if(lengthOfId == ID_VALID_LENGTH && id > 0)
       {
-          _id = id;
+          this._id = id;
       }
       else
       {
-          _id =  DEFAULT_ID;
+          this._id =  DEFAULT_ID;
       }
       // Checking if type is valid
       if(type == 'A' || type == 'B' || type == 'C' || type == 'D')
       {
-          _type = type;
+          this._type = type;
       }
       else
       {
-          _type = 'A';
+          this._type = 'A';
       }
       _brand = brand;
       _isManual = isManual;
@@ -41,30 +41,30 @@ public class Car
   // Constructor for class Car
   public Car(Car other)
   {
-      _id = other._id;
-      _type = other._type;
-      _brand = other._brand;
-      _isManual = other._isManual;
+      this._id = other._id;
+      this._type = other._type;
+      this._brand = other._brand;
+      this._isManual = other._isManual;
   }
   // Method return the id of the car
   public int getId()
   {
-      return _id;
+      return this._id;
   }
   // Method return the type of the car
   public char getType()
   {
-      return _type;
+      return this._type;
   }
   // Method return the brand of the car
   public String getBrand()
   {
-      return _brand;
+      return this._brand;
   }
   // Method return if the car is manual
   public boolean isManual()
   {
-      return _isManual;
+      return this._isManual;
   }
   // Method set the car id
   public void setId(int id)
@@ -72,11 +72,11 @@ public class Car
       int lengthOfId = String.valueOf(id).length();
       if(lengthOfId == ID_VALID_LENGTH && id > 0)
       {
-          _id = id;
+          this._id = id;
       }
       else
       {
-          _id =  id;
+          this._id = id;
       }
   }
   // Method set the car type
@@ -85,33 +85,33 @@ public class Car
       // Checking if type is valid
       if(type == 'A' || type == 'B' || type == 'C' || type == 'D')
       {
-          _type = type;
+          this._type = type;
       }
       else
       {
-          _type = type;
+          this._type = type;
       }
   }
   // Method set the car brand
   public void setBrand(String brand)
   {
-      _brand = brand;
+      this._brand = brand;
   }
   // Method set if the car is manual
   public void setIsManual(boolean manual)
   {
-      _isManual = manual;
+      this._isManual = manual;
   }
   // Method returns the car data as a string
    public String toString()
   {
-    return "id:" + _id + " type:" + _type + " brand:" + _brand + " gear:" + _isManual;
+    return "id:" + this._id + " type:" + this._type + " brand:" + this._brand + " gear:" + this._isManual;
   }
   // Method checking if two car is equals
   public boolean equals (Car other)
   {
       boolean isCarEqual = true;
-      if (_type == other._type && _brand == other._brand && Boolean.compare(_isManual, other._isManual) == 0)
+      if (this._type == other._type && this._brand == other._brand && Boolean.compare(this._isManual, other._isManual) == 0)
       {
           return isCarEqual;
       }
@@ -125,17 +125,17 @@ public class Car
   public boolean better (Car other)
   {
       boolean isCarBetter = true;
-      if(Character.compare(_type, other._type) > 0)
+      if(Character.compare(this._type, other._type) > 0)
       {
           return true;
       }
-      else if(Character.compare(_type, other._type) == 0)
+      else if(Character.compare(this._type, other._type) == 0)
       {
-          if(_isManual == true && _isManual == false)
+          if(this._isManual == true && other._isManual == false)
           {
               return true;
           }
-          else if(_isManual == true && _isManual == true)
+          else if(this._isManual == true && other._isManual == true)
           {
               return false;
           }
