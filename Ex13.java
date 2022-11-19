790c853e532629a933f9cec5f4f0b6a1823c37fd
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2022-11-19 21:12:27 +0200

    fix file

--- Ex13.java
+++ Ex13.java
@@ -3,17 +3,69 @@
 // A class with all the methods of all the exercises
 // semester סתיו 2023א
 //*******************************************************
+import java.util.Scanner;
 public class Ex13
 {
-    /**
-    * Ex1
-    * Function to return the minimum number of characters of the given binary string to be replaced to make the string alternating
-    * It can be assumed that it has N zeros and n ones and it contains only zero and one
-    * @param s (string) 
-    * @return numOfExchanges (int) 
-    * Time Complexity: O(len)
-    * Auxiliary Space: O(1)
-    */
+    public static void main(String[] args)
+    {
+       // tester for alternating method
+       //System.out.println(alternating("10"));// 0
+       //System.out.println(alternating("1100"));// 1
+       //System.out.println(alternating("0011"));// 1
+       //System.out.println(alternating("1010"));// 0
+       //System.out.println(alternating("111000"));// 1
+       //System.out.println(alternating("000111"));// 1
+       //System.out.println(alternating("11110000"));// 2
+       //System.out.println(alternating("1110001110001100"));// 3
+       // tester for maximumLengthOfSubArray
+       //int[] arr = new int[1];
+       //arr[0] = 5;
+       //System.out.println(MaximumLengthOfSubArray(arr, 1)); // 1 
+       //int[] arr1 = new int[2];
+       //arr1[0] = 4;
+       //arr1[1] = 10;
+       //System.out.println(MaximumLengthOfSubArray(arr1, 2)); // 2
+       //int[] arr2 = new int[5];
+       //arr2[0] = 4;
+       //arr2[1] = 1;
+       //arr2[2] = 2;
+       //arr2[3] = 11;
+       //arr2[4] = 9;
+       //System.out.println(MaximumLengthOfSubArray(arr2, 5)); // 4
+       // tester for isWay method 
+       //System.out.println(isWay(arr, 0)); // true
+       //System.out.println(isWay(arr1, 0)); // false
+       //System.out.println(isWay(arr2, 0)); // true
+       // tester for prince method
+       int [][] twoDArr = new int[5][5];
+       twoDArr[0][0] = 2;
+       twoDArr[0][1] = 0;
+       twoDArr[0][2] = 1;
+       twoDArr[0][3] = 2;
+       twoDArr[0][4] = 3;
+       twoDArr[1][0] = 2;
+       twoDArr[1][1] = 3;
+       twoDArr[1][2] = 5;
+       twoDArr[1][3] = 5;
+       twoDArr[1][4] = 4;
+       twoDArr[2][0] = 8;
+       twoDArr[2][1] = -1;
+       twoDArr[2][2] = 6;
+       twoDArr[2][3] = 8;
+       twoDArr[2][4] = 7;
+       twoDArr[3][0] = 3;
+       twoDArr[3][1] = 4;
+       twoDArr[3][2] = 7;
+       twoDArr[3][3] = 2;
+       twoDArr[3][4] = 4;
+       twoDArr[4][0] = 2;
+       twoDArr[4][1] = 4;
+       twoDArr[4][2] = 3;
+       twoDArr[4][3] = 1;
+       twoDArr[4][4] = 2;
+       System.out.println(prince(twoDArr, 0, 0, 0)); // 4
+    }// end of method main
+
     public static int getFlipCounter(String str, char predictedCharacter)
     {
         int flipCounter=0;
@@ -29,10 +81,19 @@ public class Ex13
         return flipCounter;
     }// end of method getFlipCounter
     
+    /**
+    * Ex1
+    * Function to return the minimum number of characters of the given binary string to be replaced to make the string alternating
+    * It can be assumed that it has N zeros and n ones and it contains only zero and one
+    * @param s (string) 
+    * @return numOfExchanges (int) 
+    * Time Complexity: O(len)
+    * Auxiliary Space: O(1)
+    */
     public static int alternating(String str)
     {
         return Math.min(getFlipCounter(str,'1'), getFlipCounter(str,'0')) / 2;
-    }// end of exercise 1
+    }// end of method alternating
     
     /**
     * Ex2
@@ -47,18 +108,18 @@ public class Ex13
     static int MaximumLengthOfSubArray(int a[], int n)
     {
         int sum = 0, len = 0;
- 
+    
         // Check if sum of complete array is even
         for (int i = 0; i < n; i++)
         {
             sum += a[i];
         }
- 
+    
         if (sum % 2 == 0) // total sum is already even
         {
             return n;
         }
- 
+    
         // Find an index i such the a[i] is odd
         // and compare length of both halfs excluding
         // a[i] to find max length subarray
@@ -69,22 +130,22 @@ public class Ex13
                 len = Math.max(len, Math.max(n - i - 1, i));
             }
         }
- 
+    
         return len;
     }// end of exercise 2
     /**
     * Ex3
     * The method is boolean recursive which returns if there is a valid route in the array
     * A valid path in an array is defined as a series of indices in the array starting from zero and moving to the right or left depending on the value in the cell when it is forbidden to leave the array's boundaries
-    * @param a (int arr) 
+    * @param arr, int 
     * @return true if there valid route or false if not
     */
-   public static boolean isWay(int[] a, int index)
-   {
-    if (index == a.length-1) return true;
-    if (index > a.length-1 || index < 0) return false;
-    return isWay(a, index+a[index]) || isWay(a, index-a[index]);
-   }
+    public static boolean isWay(int[] a, int index)
+    {
+        if (index == a.length-1) return true;
+        if (index > a.length-1 || index < 0) return false;
+        return isWay(a, index+a[index]) || isWay(a, index-a[index]);
+    }// end of method isWay
     /**
     * Ex4
     * The method is Boolean recursive and returns the smalest number of steps that the prince need to do to the evil person (-1) in two-dimensional array, If there is no path, then it returns -1
@@ -92,58 +153,46 @@ public class Ex13
     * @param drm (int [][]), i (int), j (int) 
     * @return the numbers of steps to get to the evil person and if there no such legal route to him the method will return -1
     */
-    public static int prince(int[][] drm, int i, int j)
+    public static int prince(int[][] drm, int i, int j, int count)
     {
-        int count = 0;
         //Checking if the cell he arrived at is the desired cell  
         if(drm[i][j] == -1)
         {
             return count;
         }
-        if ( i + 1 > drm.length)
+        else if(
+        if ( i + 1 = drm.length)
         {
             drm[i + 1][j] += drm[i][j] + 5;
-        }
-        else if( i - 1 < 0 )
-        {
-            drm[i - 1][j] += drm[i][j] + 5;
-        }
-        else if(j + 1 > drm[0].length)
-        {
-            drm[i][j + 1] += drm[i][j] + 5;
-        }
-        else if(j - 1 < 0)
-        {
-            drm[i][j - 1] += drm[i][j] + 5;
         }
         else if(drm[i][j] == drm[i + 1][j] || drm[i + 1][j] == drm[i + 1][j] + 1 || drm[i + 1][j] == drm[i + 1][j] -1 || drm[i + 1][j] == drm[i + 1][j] -2)
         {
             count++;
-            prince(drm, i + 1, j);
+            prince(drm, i + 1, j, count);
             drm[i + 1][j] += drm[i][j] + 5;
-            prince(drm, i, j);
+            prince(drm, i, j, count);
         }
         else if(drm[i][j] == drm[i - 1][j] || drm[i - 1][j] == drm[i][j] + 1 || drm[i - 1][j] == drm[i][j] -1 || drm[i - 1][j] == drm[i][j] -2)
         {
             count++;
-            prince(drm, i - 1, j);
+            prince(drm, i - 1, j, count);
             drm[i - 1][j] += drm[i][j] + 5;
-            prince(drm, i, j);
+            prince(drm, i, j, count);
         }
         else if(drm[i][j] == drm[i][j +1] || drm[i][j + 1] == drm[i][j] + 1 || drm[i][j + 1] == drm[i][j] -1 || drm[i][j + 1] == drm[i][j] -2)
         {
             count++;
-            prince(drm, i, j + 1);
+            prince(drm, i, j + 1, count);
             drm[i][j + 1] += drm[i][j] + 5;
-            prince(drm, i, j);
+            prince(drm, i, j, count);
         }
         else if(drm[i][j] == drm[i][j - 1] || drm[i][j - 1] == drm[i][j] + 1 || drm[i][j - 1] == drm[i][j] -1 || drm[i][j - 1] == drm[i][j] -2)
         {
             count++;
-            prince(drm, i, j - 1);
+            prince(drm, i, j - 1, count);
             drm[i][j - 1] += drm[i][j] + 5;
-            prince(drm, i, j);
+            prince(drm, i, j, count);
         }
-        return -1;
-    } // end of exercise 4
+        return count;
+      }// end of mehtod prince
 }// end of class Ex13
