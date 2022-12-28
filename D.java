d36eb2e014fedc38805ee286b5d0e25cf4bc731f
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2022-12-28 14:41:00 +0200

    add D.java file

---
+++ D.java
@@ -0,0 +1,13 @@
+public class D extends C
+{
+    public void foo(C c)
+    {
+        System.out.println("dc");
+    }// end of methdo foo
+    
+    public void foo(D d)
+    {
+        System.out.println("dd");
+    }
+    
+}// end of method class D
