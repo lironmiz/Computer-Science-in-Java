48c92ca64579fb5e28275b2341a1d0e9e83e4d98
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2022-12-28 14:46:02 +0200

    Create A4.java

---
+++ A4.java
@@ -0,0 +1,33 @@
+public class A
+{
+    private int _x;
+    public A(int x)
+    {
+        _x = x;
+    }// end of method A
+    
+    public int getX()
+    {
+        return _x;
+    }// end of method  getX
+    
+    public int doubleX()
+    {
+        return 2 * getX();
+    }// end of method  doubleX
+    
+    public int tripleX()
+    {
+        return 3 * _x;
+    }// end of method tripleX
+    
+    public int subXhelper()
+    {
+        return _x -1;
+    }// end of method subXhelper
+    
+    public int subX()
+    {
+        return subXhelper();
+    }// end of method subX
+}// end of class A
