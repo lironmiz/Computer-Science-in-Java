920c422b40241cf953b62988f735fdd2ae240b28
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-27 07:10:35 +0200

    Update ClassB.java

--- ClassB.java
+++ ClassB.java
@@ -1,17 +1,14 @@
+//*******************************************************
+// ClassB.java
+// the class reprsents ClassB
+// Author: liron mizrahi
+//*******************************************************
 public class ClassB extends ClassA
-
 {
-
     public ClassB() {} 
-
-
-
+    
     public void f()
-
     {
-
         System.out.println("Class b");
-
     }
-
-}
+}// end of class ClassB
