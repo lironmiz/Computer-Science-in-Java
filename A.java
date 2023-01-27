d9fc88b23096d16be93b81f661dcd55844843c80
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-27 06:32:00 +0200

    Update A.java

--- A.java
+++ A.java
@@ -1,5 +1,9 @@
-public class A {
-
+//*******************************************************
+// A.java
+// the class reprsents A
+// Author: liron mizrahi
+//*******************************************************
+public class A
+{
     public void method1(float x){ … }
-
-}
+}// end of method A
