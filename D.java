020496344f25645c6b08c68259318e4b67275da0
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-27 07:37:30 +0200

    Update D.java

--- D.java
+++ D.java
@@ -1,3 +1,8 @@
+//*******************************************************
+// D.java
+// the class reprsents D
+// Author: liron mizrahi
+//*******************************************************
 public class D extends C
 {
     public void foo(C c)
@@ -8,6 +13,5 @@ public class D extends C
     public void foo(D d)
     {
         System.out.println("dd");
-    }
-    
+    } // end of method foo
 }// end of method class D
